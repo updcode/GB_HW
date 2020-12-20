a76a93d91251fc27331b1b223273d99efedcef76
Pavel <upd.code@gmail.com>
2020-12-20 14:58:24 +0300

    add Homework_05

---
+++ Lesson_05/Homework_05_Task_01/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Homework_05_Task_01
+{
+    class Program
+    {
+        static bool VerifyLogin(string login)
+        {
+            if (login.Length >= 2 && login.Length <= 10)
+            {
+                bool flag = true;
+                char symbol = login[0];
+
+                if (Char.IsDigit(symbol)) 
+                {
+                    return false;
+                }
+                    
+                for (int i = 1; i < login.Length; i++)
+                {
+                    symbol = login[i];
+
+                    if (!(Char.IsDigit(symbol) || VerifyLatinSymbol(symbol)))
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+
+                if (flag) 
+                {
+                    return true;
+                }      
+            }
+
+            return false;
+        }
+
+        static bool VerifyLoginRegular(string login)
+        {
+            char symbol = login[0];
+
+            if (Char.IsDigit(symbol))
+            {
+                return false;
+            }
+
+            if (!Regex.IsMatch(login, @"^[a-zA-Z0-9]+${2,10}")) 
+            {
+                return false;
+            }
+                
+            return true;
+        }
+
+        private static bool VerifyLatinSymbol(char symbol)
+        {
+            var numbers = symbol;
+
+            if ((numbers >= 65 && numbers <= 90) || (numbers >= 97 && numbers <= 122))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }     
+        }
+
+        static void Main(string[] args)
+        {
+            /* Homework_05_Task_01 Derushko Pavel
+             * Создать программу, которая будет проверять корректность ввода логина.
+             * Корректным логином будет строка от 2 до 10 символов,
+             * содержащая только буквы латинского алфавита
+             * или цифры, при этом цифра не может быть первой:
+             * а) без использования регулярных выражений;
+             * б) **с использованием регулярных выражений.
+             */
+
+            while (true)
+            {
+                Console.Write("Input login: ");
+                var login = Console.ReadLine();
+
+                if (VerifyLogin(login) && VerifyLoginRegular(login))
+                {
+                    Console.WriteLine($"Login is correct. Welcome, {login}!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid login or password. Try again.");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson_05/Homework_05_Task_03/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_05_Task_03
+{
+    class Program
+    {
+        static bool permuitationWord(string str1, string str2)
+        {
+            return str1.Select(Char.ToUpper).OrderBy(x => x).SequenceEqual(str2.Select(Char.ToUpper).OrderBy(x => x));
+        }
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Input first word: ");
+                var s1 = Console.ReadLine();
+
+                Console.Write("Input second word: ");
+                var s2 = Console.ReadLine();
+
+                if (permuitationWord(s1, s2) == true)
+                {
+                    Console.WriteLine($"{s1} is a permutation of {s2}");
+                }
+                else
+                {
+                    Console.WriteLine($"{s1} is not a permutation of {s2}");
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
